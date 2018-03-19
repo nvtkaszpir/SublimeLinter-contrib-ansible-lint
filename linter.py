ae30e402f0511964076d5d776e77880bec2e2fc2
Michał Sochoń <kaszpir@gmail.com>
2018-03-19 11:59:09 +0100

    Add fitering of the utput to match only currently viewed file, (it still has issues, though)

--- linter.py
+++ linter.py
@@ -9,8 +9,10 @@
 #
 
 """This module exports the AnsibleLint plugin class."""
-
+import re
+import logging
 from SublimeLinter.lint import Linter, util
+logger = logging.getLogger('SublimeLinter.plugin.ansbile-lint')
 
 
 class AnsibleLint(Linter):
@@ -45,3 +47,30 @@ class AnsibleLint(Linter):
         '-x': '',
     }
     inline_overrides = ['c', 'exclude', 'r', 'R', 't', 'x']
+
+    def find_errors(self, output):
+        """Parse errors from linter's output.
+        Display only errors for matching file
+        ansible-lint parses given playbook and its dependent roles
+
+        # TODO: add ansible dependencies, so that we detect playbooks and then
+        # iterate over them to detect if given open file belongs to the role
+        # included in the playbook
+        # if yes, then run ansible-lint on those playbooks and process output
+
+        """
+        pattern = r'^{}(.*)'.format(re.escape(self.filename))
+
+        for line in output.splitlines():
+
+            # filter out lines which do not match currently opened file
+            out = re.match(pattern, line)
+            if not out:
+                continue
+
+            found = self.regex.match(line.rstrip())
+            if found:
+                yield self.split_match(found)
+            else:
+                logger.info(
+                    "{}: No match for line: '{}'".format(self.name, line))
